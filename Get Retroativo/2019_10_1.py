9713a885baa06c277a3ad3210c26fec491d8ddff
Paulo Mota <paaulowiz@gmail.com>
2019-11-28 01:11:32 -0300

    Executado mês de novembro

--- Get Retroativo/2019_10_1.py
+++ Get Retroativo/2019_10_1.py
@@ -15,13 +15,16 @@ print('-')
 """
 robo5 = rr.RobotRotine()
 i = 0
-dtinit = '2019-10-01T00:00:00.645154'
+dtinit = '2019-11-01T00:00:00.00000'
 while i < 15:
     print(dtinit)
-    dtfim = np.datetime64(dtinit) + np.timedelta64(24, 'h')
-    robo5.ExecutaRotina('created_at', dtinit,
+    try:
+        dtfim = np.datetime64(dtinit) + np.timedelta64(24, 'h')
+        robo5.ExecutaRotina('date_approved', dtinit,
                         dtfim, 1)
-    i = i+1
-    dtinit = np.datetime64(dtinit) + np.timedelta64(24, 'h')
+        i = i+1
+        dtinit = np.datetime64(dtinit) + np.timedelta64i(24, 'h')
+    except:
+        print('deu erro')
 
 print('Periodo Executado com sucesso')
